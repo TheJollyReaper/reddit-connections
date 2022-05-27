c96aae2a39c5969c9dee21cf7a15083c427ee23d
Addie Bjornson <addiebjornson@gmail.com>
2022-05-26 18:21:10 -0700

    pairwise data  is done! hopefully

--- filter_pairwise.py
+++ filter_pairwise.py
@@ -1,27 +1,31 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from sympy import maximum
 
 pairwise = pd.read_csv("datasets/pairwise_variables.csv")
+small_pairwise = pd.read_csv("datasets/small_pairwise.csv")
 attributes = pd.read_csv("datasets/subreddit_attributes.csv")
+
+
+# below, filters out pairs that have a weaker mutualism and competition
+ESTIMATE_THRESHOLD = 0.127
 
 # below, filters out pairs that have a lower number of cross-posts
 CROSS_POSTING_THRESHOLD = 20
 
 # below, filters out pairs that have a lower author similarity
-AUTHOR_SIMILARITY_THRESHOLD = 0.90
+AUTHOR_SIMILARITY_THRESHOLD = 0.98
 
 # below, filters out pairs that have a lower term similarity
-TERM_SIMILARITY_THRESHOLD = 0.90
+TERM_SIMILARITY_THRESHOLD = 0.99
 
 
 
-## ESTIMATE (HOVER TAGS AND LINES)
+## ESTIMATE
 # This code creates two csv files in the datasets folder
 # 1. "estimates_full.csv" has a column of the text of the hover tag in it for competition/mutualism
 # 2. "estimates_lines.csv" is the same but only includes subreddits where the estimate
-#     has a z-score above 2 or below -2. These are the only lines that are going to be
+#     that is in the 5th highest and lowest percentiles. These are the only lines that are going to be
 #     shown when the user clicks "competition/mutualism" for lines.
 
 
@@ -30,85 +34,125 @@ TERM_SIMILARITY_THRESHOLD = 0.90
 estimates_full = pairwise
 estimates_full['new_estimate'] = estimates_full.loc[estimates_full['Std. Error'] < 0.05, 'Estimate']
 estimates_full = estimates_full.loc[estimates_full['new_estimate'].notnull()]
-estimates_full['new_estimate_scaled'] = (estimates_full['new_estimate'] - estimates_full['new_estimate'].mean()) / estimates_full['new_estimate'].std()  
+estimates_full['new_estimate'] = round(estimates_full['new_estimate'], 4)
 
-# Define intervals based on z-score and label them in the column "hover tag"
+quants = list(round(estimates_full['new_estimate'].quantile([0, 0.05, 0.25,0.45,0.75,0.95, 1.0]), 4))
+zero = np.quantile(estimates_full['new_estimate'], .45)
+
 conditions = [
-    (estimates_full['new_estimate_scaled'] > 3),
-    (estimates_full['new_estimate_scaled'] <= 3) & (estimates_full['new_estimate_scaled'] > 2),
-    (estimates_full['new_estimate_scaled'] <= 2) & (estimates_full['new_estimate_scaled'] > 1),
-    (estimates_full['new_estimate_scaled'] <= 1) & (estimates_full['new_estimate_scaled'] > 0),
-    (estimates_full['new_estimate_scaled'] <= 0) & (estimates_full['new_estimate_scaled'] > -1),
-    (estimates_full['new_estimate_scaled'] <= -1) & (estimates_full['new_estimate_scaled'] > -2),
-    (estimates_full['new_estimate_scaled'] <= -2) & (estimates_full['new_estimate_scaled'] > -3),
-    (estimates_full['new_estimate_scaled'] <= -3)
+    (estimates_full['new_estimate'] <= quants[6]) & (estimates_full['new_estimate'] > quants[5]),
+    (estimates_full['new_estimate'] <= quants[5]) & (estimates_full['new_estimate'] > quants[4]),
+    (estimates_full['new_estimate'] <= quants[4]) & (estimates_full['new_estimate'] > quants[3]),
+    (estimates_full['new_estimate'] <= quants[3]) & (estimates_full['new_estimate'] > quants[2]),
+    (estimates_full['new_estimate'] <= quants[2]) & (estimates_full['new_estimate'] > quants[1]),
+    (estimates_full['new_estimate'] <= quants[1]) & (estimates_full['new_estimate'] >= quants[0]),
 ]
 
+
 choices = [
-    'most mutualistic',
     'very mutualistic',
     'somewhat mutualistic',
     'a little mutualistic',
     'a little competitive',
     'somewhat competitive',
     'very competitive',
-    'most competitive']
+    ]
 
 estimates_full['hover_tag'] = np.select(conditions, choices)
+estimates_full = estimates_full[['Subreddit.j', 'Subreddit.i', 'new_estimate', 'hover_tag' ]]
+
 
 # Create a dataframe only of the most competitive and mutualistic rows
-estimates_lines = estimates_full[np.abs(estimates_full['new_estimate_scaled']) > 2]
+estimate_lines = estimates_full[np.abs(estimates_full['new_estimate']) > ESTIMATE_THRESHOLD]
+
 
 # export dataframes to csv
 estimates_full.to_csv("datasets/estimates_full.csv")
-estimates_lines.to_csv("datasets/estimates_lines.csv")
+estimate_lines.to_csv("datasets/estimates_lines.csv")
 
 
 # CROSS POSTING
-# This code creates a csv file in the datasets folder to show which lines are shown whtn the variable is chosen for lines.
-# This was tricky because the pandas grouby() was finicky but it works, only omits data poorly when more than 2 subreddits in the 
-# same cluster share the same number of crossposts, but this is very seldom.
-# 1. Rename the key column
-# 2. Create a duplicate dataframe of the subreddits, key, and crossposts
-# 3. Ensure the subreddits have over 50 crossposts
-# 4. groupby() # of crossposts and the cluster id
-# 5. merge the extra dataframe back in
-# 6. rename the columns so subreddits j and i are named as they were at the beginning
-# 7. Drop extra columns
-# 8. standardize the cross-posting values and cap them at +3 and -3
-# 8. Save as csv
+# This code creates two csv files in the datasets folder
+# 1. "cross_posting_full.csv" has a column of the number of cross posts
+# 2. "cross_posting_full_lines.csv" is the same but only includes subreddits where 
+    # there are a LOT of cross_posts
 
-key_fix = pairwise
-key_fix['key'] = key_fix['Unnamed: 0']
-cp_base = key_fix
-cp_extra = key_fix[['key', 'Subreddit.i', 'Subreddit.j', 'cross_posts']]
-
-cp_base = cp_base[cp_base['cross_posts'] > CROSS_POSTING_THRESHOLD]
-cp_base = cp_base.groupby(["cross_posts", 'clid']).max()
-
-cp_both = pd.merge(cp_base, cp_extra, on='key')
-
-cp_both['Subreddit.j'] = cp_both['Subreddit.j_y']
-cp_both['Subreddit.i'] = cp_both['Subreddit.i_y']
-
-cp_both = cp_both.drop(columns =['Subreddit.i_y', 'Subreddit.j_y', 'Subreddit.i_x', 'Subreddit.j_x', 'key'])
-
-cp_both['cross_posts_scaled'] = (cp_both['cross_posts'] - cp_both['cross_posts'].mean()) / cp_both['cross_posts'].std() 
-cp_both['cross_posts_scaled'] = np.minimum(cp_both['cross_posts_scaled'], 3)
-cp_both['cross_posts_scaled'] = np.maximum(cp_both['cross_posts_scaled'], -3)
-
-cp_both.to_csv("datasets/cross_posting_lines.csv")
+cp = small_pairwise
+cp = cp[['Subreddit.j', 'Subreddit.i', 'cross_posts']]
+cp.to_csv("datasets/cross_posting.csv")
+cp = cp[cp['cross_posts'] > CROSS_POSTING_THRESHOLD]
+cp.to_csv("datasets/cross_posting_lines.csv")
 
 
 
 # AUTHOR SIMILARITY
 
-auth = pairwise
-auth = auth[auth['author_similarity'] > AUTHOR_SIMILARITY_THRESHOLD]
+auth = small_pairwise
+
+quants = list(round(auth['author_similarity'].quantile([0.25, 0.50, 0.75, 1.0]), 4))
+auth['author_similarity'] = round(auth['author_similarity'], 4)
+
+conditions = [
+    (auth['author_similarity'] <= quants[3]) & (auth['author_similarity'] > quants[2]),
+    (auth['author_similarity']<= quants[2]) & (auth['author_similarity'] > quants[1]),
+    (auth['author_similarity']  <= quants[1]) & (auth['author_similarity'] > quants[0]),
+    (auth['author_similarity'] <= quants[0]) & (auth['author_similarity'] > 0),
+    (auth['author_similarity'] <= 0)
+]
+
+choices = [
+    'very high author similarity',
+    'high author similarity',
+    'some author similarity',
+    'a little author similarity',
+    'no author similarity'
+    ]
+
+auth['hover_tag'] = np.select(conditions, choices)
+auth = auth[['Subreddit.j', 'Subreddit.i', 'author_similarity', 'hover_tag' ]]
+
+# Create a dataframe only of the most competitive and mutualistic rows
+author_lines = auth[np.abs(auth['author_similarity']) > AUTHOR_SIMILARITY_THRESHOLD]
+
+# export dataframes to csv
 auth.to_csv("datasets/author_similarity.csv")
+author_lines.to_csv("datasets/author_lines.csv")
+
 
 # TERM SIMILARITY
+term = small_pairwise
 
-term = pairwise
-term = term[term['term_similarity'] > TERM_SIMILARITY_THRESHOLD]
+quants = list(round(term['term_similarity'].quantile([0.25, 0.50, 0.75, 1.0]), 4))
+term['term_similarity'] = round(term['term_similarity'], 4)
+
+conditions = [
+    (term['term_similarity'] <= quants[3]) & (term['term_similarity'] > quants[2]),
+    (term['term_similarity']<= quants[2]) & (term['term_similarity'] > quants[1]),
+    (term['term_similarity']  <= quants[1]) & (term['term_similarity'] > quants[0]),
+    (term['term_similarity'] <= quants[0]) & (term['term_similarity'] > 0),
+    (term['term_similarity'] <= 0)
+]
+
+
+choices = [
+    'very high term similarity',
+    'high term similarity',
+    'some term similarity',
+    'a little term similarity',
+    'no term similarity'
+    ]
+
+term['hover_tag'] = np.select(conditions, choices)
+term = term[['Subreddit.j', 'Subreddit.i', 'term_similarity', 'hover_tag']]
+
+
+# export dataframes to csv
 term.to_csv("datasets/term_similarity.csv")
+
+term_lines = term[term['term_similarity'] > TERM_SIMILARITY_THRESHOLD]
+term_lines.to_csv("datasets/term_similarity_lines.csv")
+
+
+print('term: ' + str(len(term_lines)))
+print('author: ' + str(len(author_lines)))
+print('cross posts: ' + str(len(cp)))
